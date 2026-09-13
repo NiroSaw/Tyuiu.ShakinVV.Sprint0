dd76844fac8d2484ff5f8df46704cc8b5e6cb227
NiroSaw <vovanvova226@gmail.com>
2026-09-13 15:24:14 +0500

    Add Task5

---
+++ Tyuiu.ShakinVV.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShakinVV.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShakinVV.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6f9f2d64-ea3d-49f9-a269-0dc5c6f12cd1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ShakinVV.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShakinVV.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.ShakinVV.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Линейная структура
+            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+            Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
+            Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+
+            //Разветвляющаяся структура в библиотеке классов Division
+            Console.WriteLine("A / B = " + DataService.Division(9, 3));
+            Console.ReadKey();
+
+        }
+    }
+}
